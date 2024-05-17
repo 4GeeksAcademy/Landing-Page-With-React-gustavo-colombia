a07884919faa405178d2e4d7858d561e388e6056
GustavoColombia <157833803+GustavoColombia@users.noreply.github.com>
2024-05-17 17:44:14 +0000

    imagen in card

--- src/js/component/Card.jsx
+++ src/js/component/Card.jsx
@@ -4,7 +4,7 @@ import React from "react";
 const Card = () => {
     return (
 		<div className="card" style= {{width: "18rem"}}>
-  <img src="" className="card-img-top" alt="..."/>
+  <img src="https://e00-marca.uecdn.es/assets/multimedia/imagenes/2023/06/17/16869958171241.jpg" className="card-img-top" alt="..."/>
   <div className="card-body mt-5">
     <h5 className="card-title">Card title</h5>
     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
